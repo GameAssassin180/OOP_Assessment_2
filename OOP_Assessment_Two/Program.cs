08acaffc4ae1247463f2c22474dc3791a7b357af
Mitchell Seng <MitchellSeng02@gmail.com>
2021-03-24 11:50:41 +0000

    First commit no code added yet, added classes before next commit

---
+++ OOP_Assessment_Two/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Assessment_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
