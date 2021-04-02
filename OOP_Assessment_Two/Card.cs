98664e7fc10ea7cd63c3a921aeba825ea3912f95
Mitchell Seng <MitchellSeng02@gmail.com>
2021-04-02 10:44:40 +0100

    Added code to the card class, not tested yet next commit will state if this worked.

--- OOP_Assessment_Two/Card.cs
+++ OOP_Assessment_Two/Card.cs
@@ -6,7 +6,53 @@ namespace OOP_Assessment_Two
 {
     class Card
     {
-        string suit;
-        int value;
+        public string[] suite = { "Diamond", "Spade", "Heart", "Clubs" };
+
+        public int Value
+        {
+            get;
+            set;
+        }
+
+        public string pictureCards
+        {
+            get
+            {
+                string name = string.Empty;
+                switch (Value)
+                {
+                    case (1):
+                        name = "Ace";
+                        break;
+                    case (11):
+                        name = "Jack";
+                        break;
+                    case (12):
+                        name = "Queen";
+                        break;
+                    case (13):
+                        name = "King";
+                        break;
+                    default:
+                        name = Value.ToString();
+                        break;
+                }
+                return name;
+            }
+        }
+
+        public string cardName
+        {
+            get
+            {
+                return pictureCards + " of " + suite.ToString();
+            }
+        }
+
+        public Card(int Value, string[] suite)
+        {
+            this.Value = Value;
+            this.suite = suite;
+        }
     }
 }
